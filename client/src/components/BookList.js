f3f7f1db2f7ca255c18740f1e20629167996d987
Mijeong Ban <mban1@stevens.edu>
2019-11-10 13:31:12 -0500

    added comment

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
-import { getBooksQuery } from "../queries/queries";
+import { getBooksQuery } from "../queries/queries"; /* import getBooksQuery */
 import BookDetails from "./BookDetails";
 
 class BookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: null
+      selected: null /* Selected status for showing book details */
     };
   }
   displayBooks() {
     var data = this.props.data;
+    console.log(data);
     if (data.loading) {
       return <div>Loading books...</div>;
     } else {
+      /* If data was loaded */
       return data.books.map(book => {
         return (
           <li
             key={book.id}
             onClick={e => {
-              this.setState({ selected: book.id });
+              this.setState({
+                selected: book.id
+              }); /* put book Id for selected status */
             }}
           >
             {book.name}
@@ -33,6 +37,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        {/* pass selected state into BookDetails component */}
         <BookDetails bookId={this.state.selected} />
       </div>
     );
